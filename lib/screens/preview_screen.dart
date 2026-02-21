ed87cd6295a51600896756b81bcfce0b38a1899b
Rocky Shi <bangtty@gmail.com>
2026-02-21 20:10:38 +0800

    Add Escape key to dismiss preview window
    Wrap PreviewScreen in a KeyboardListener so pressing Esc
    triggers the same discard flow as the toolbar button.

--- lib/screens/preview_screen.dart
+++ lib/screens/preview_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../state/app_state.dart';
 import '../widgets/preview_toolbar.dart';
 
-class PreviewScreen extends StatelessWidget {
+class PreviewScreen extends StatefulWidget {
   final AppState appState;
   final VoidCallback onCopy;
   final VoidCallback onSave;
@@ -18,34 +19,57 @@ class PreviewScreen extends StatelessWidget {
   });
 
   @override
+  State<PreviewScreen> createState() => _PreviewScreenState();
+}
+
+class _PreviewScreenState extends State<PreviewScreen> {
+  final _focusNode = FocusNode();
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return ListenableBuilder(
-      listenable: appState,
+      listenable: widget.appState,
       builder: (context, _) {
-        final bytes = appState.screenshotBytes;
+        final bytes = widget.appState.screenshotBytes;
         if (bytes == null) {
           return const ColoredBox(color: Color(0xFF1E1E1E));
         }
-        return Stack(
-          fit: StackFit.expand,
-          children: [
-            // Screenshot fills the entire window
-            Image.memory(bytes, fit: BoxFit.contain, gaplessPlayback: true),
+        return KeyboardListener(
+          focusNode: _focusNode,
+          autofocus: true,
+          onKeyEvent: (event) {
+            if (event is KeyDownEvent &&
+                event.logicalKey == LogicalKeyboardKey.escape) {
+              widget.onDiscard();
+            }
+          },
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              // Screenshot fills the entire window
+              Image.memory(bytes, fit: BoxFit.contain, gaplessPlayback: true),
 
-            // Floating toolbar at bottom center
-            Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: Center(
-                child: PreviewToolbar(
-                  onCopy: onCopy,
-                  onSave: onSave,
-                  onDiscard: onDiscard,
+              // Floating toolbar at bottom center
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Center(
+                  child: PreviewToolbar(
+                    onCopy: widget.onCopy,
+                    onSave: widget.onSave,
+                    onDiscard: widget.onDiscard,
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         );
       },
     );
